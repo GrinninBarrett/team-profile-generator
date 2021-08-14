3b842be4867fa5ee85c71bd8ae30e823ec470aff
Tucker Barrett <tuckerbarrett@gmail.com>
2021-08-14 18:48:21 -0400

    Updates intial function in index.js

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const Intern = require("./lib/Intern");
 const fs = require("fs");
 const inquirer = require("inquirer");
 
+let fullTeam = [];
 
 function addManager() {
     return inquirer
         .prompt([
             {
                 type: "input",
-                name: "manager",
+                name: "name",
                 message: "What is the team manager's name?"
             },
             {
@@ -31,41 +32,13 @@ function addManager() {
             }
         ])
         .then((response) => {
-            const  { manager, id, email, officeNumber } = response;
-            const manager = new Manager (manager, id, email, officeNumber);
-        })
-}
-
-function init() {
-    inquirer
-        .prompt([
-            {
-                type: "input",
-                name: "manager",
-                message: "What is the team manager's name?"
-            },
-            {
-                type: "input",
-                name: "id",
-                message: "What is the manager's employee ID?"
-            },
-            {
-                type: "input",
-                name: "email",
-                message: "What is the manager's email address?"
-            },
-            {
-                type: "input",
-                name: "office",
-                message: "What is the manager's office number?"
-            }
-        ])
-        .then((response) => {
-            const manager = new Manager(response.manager.trim(), response.id.trim(), response.email.trim(), response.office.trim());
+            const  { name, id, email, officeNumber } = response;
+            const manager = new Manager (name, id, email, officeNumber);
             console.log(manager);
-            console.log(manager.getRole())
+            fullTeam.push(manager);
+            console.log(fullTeam);
         })
-
 }
 
-init();
+
+addManager();
