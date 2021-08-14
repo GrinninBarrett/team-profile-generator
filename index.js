18fc8fb86f7df99652c0c95a2d533508ce0be42a
Tucker Barrett <tuckerbarrett@gmail.com>
2021-08-14 18:22:43 -0400

    Updates subclasses' methods

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
-const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
 const fs = require("fs");
 const inquirer = require("inquirer");
+
+
+function addManager() {
+    return inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "manager",
+                message: "What is the team manager's name?"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is the manager's employee ID?"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is the manager's email address?"
+            },
+            {
+                type: "input",
+                name: "officeNumber",
+                message: "What is the manager's office number?"
+            }
+        ])
+        .then((response) => {
+            const  { manager, id, email, officeNumber } = response;
+            const manager = new Manager (manager, id, email, officeNumber);
+        })
+}
 
 function init() {
     inquirer
@@ -39,5 +69,3 @@ function init() {
 }
 
 init();
-
-
