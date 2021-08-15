4980fe144de4fe8da1dc7f01374d48b99280d346
Tucker Barrett <tuckerbarrett@gmail.com>
2021-08-15 14:52:32 -0400

    Adds functions to create cards and generate full html

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,3 +1,5 @@
+const Employee = require("../lib/Employee");
+
 // Creates card for Manager
 const managerCard = `
 
@@ -57,40 +59,71 @@ const internCard = `
 `;
 
 
-// Writes entire HTML file after all other pieces have been put together
-const finalDocument = `
+// Function to combine all added employee cards
+function combineCards(fullTeam) {
 
-<!DOCTYPE html>
-<html lang='en'>
+    const cardsArray = fullTeam.map(employee => {
 
-<head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-    <link href='../dist/style.css' rel='stylesheet'>
-    <title>My Team</title>
-</head>
+        // Check role for each employee in fullTeam array using the getRole() method
+        let role = employee.getRole();
 
-<body>
+        if (role === "Manager") {
+            return managerCard;
+        }
 
-    <header class="bg-success bg-gradient p-5">
-        <h1 class="text-center text-light">My Team</h1>
-    </header>
+        if (role === "Engineer") {
+            return engineerCard;
+        }
 
-    <main>
-        <div class="container-fluid row align-items-center justify-content-center m-0 p-5">
-            
-        </div>
-    </main>
+        if (role === "Intern") {
+            return internCard;
+        }
+    });
+
+    const employeeCards = cardsArray.join("");
+
+    return employeeCards;
+}
 
 
-</body>
+// Function to write entire HTML file after all other pieces have been put together
+function finalDocument(employeeCards) {
+    return `
+    <!DOCTYPE html>
+    <html lang='en'>
+    
+    <head>
+        <meta charset='utf-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+        <link href='../dist/style.css' rel='stylesheet'>
+        <title>My Team</title>
+    </head>
+    
+    <body>
+    
+        <header class="bg-success bg-gradient p-5">
+            <h1 class="text-center text-light">My Team</h1>
+        </header>
+    
+        <main>
+            <div class="container-fluid row align-items-center justify-content-center m-0 p-5">
+                ${employeeCards}
+            </div>
+        </main>
+    
+    </body>
+    
+    </html>
+    
+    `;
+}
 
-</html>
-`;
+
+
 
 // TODO: Update this file with template literals of card elements for each employee type
 // as well as a function to actually write the HTML (one card per employee) based on employee type,
