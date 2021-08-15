eb96c211d45a36b9a5ce482aa323b081edd02330
Tucker Barrett <tuckerbarrett@gmail.com>
2021-08-15 16:40:49 -0400

    Creates function to add employee in index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ let fullTeam = [];
 
 // Upon starting the application, add a manager
 function addManager() {
-    return inquirer
+    inquirer
         .prompt([
             {
                 type: "input",
@@ -50,14 +50,85 @@ function addManager() {
         })
 }
 
-// TODO: Add function to add employee based on role or exit (finish and write HTML file)
+
+// Add employees until Manager exits application
+function addEmployee() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "role",
+                message: "What is the employee's role?",
+                choices: ["Engineer", "Intern"]
+            },
+            {
+                type: "input",
+                name: "name",
+                message: "What is the employee's name?", 
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is the employee's ID?",
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is the employee's email address?",
+            },
+            {
+                type: "input",
+                name: "gitHub",
+                message: "What is the engineer's GitHub username?",
+                when: (input) => input.role === "Engineer",
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "What is the name of the intern’s school?",
+                when: (input) => input.role === "Intern",
+            },
+            {
+                type: "confirm",
+                name: "addAnotherEmployee",
+                message: "Would you like to add another employee?",
+                default: false
+            }
+        ])
+        .then((response) => {    
+
+            // Destructuring assignment of variables from adding an employee
+            const { name, id, email, role, gitHub, school, addAnotherEmployee } = response; 
+            let employee; 
+    
+            // Create new instance of subclasses based on their role
+            if (role === "Engineer") {
+                employee = new Engineer (name, id, email, gitHub);    
+            } else if (role === "Intern") {
+                employee = new Intern (name, id, email, school);
+            }
+    
+            // Push the added employee to the fullTeam array
+            fullTeam.push(employee); 
+    
+            // Run again if the manager wants to add more employees, return completed array if not
+            if (addAnotherEmployee) {
+                return addEmployee(fullTeam); 
+            } else {
+                return fullTeam;
+            }
+        })
+}
 
 
-
-// This init function was made to simply follow convention - using a more appropriately-named
-// function - addManager() - is to help with clarity
+// Upon starting the application run the appropriate functions until ending
 function init() {
-    addManager();
+    addManager()
+        .then (addEmployee)
+        .then (fullTeam => {
+            return generateHTML(fullTeam);
+        })
+
 }
 
 init();
