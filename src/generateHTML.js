50819f88e1797c475f79777b25063b57a0bf6c15
Tucker Barrett <tuckerbarrett@gmail.com>
2021-08-16 20:06:06 -0400

    Adds target of blank to engineer github link

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -37,7 +37,7 @@ function createEngineerCard(engineer) {
                     <ul class="list-group list-group-flush">
                         <li class="list-group-item">ID: ${engineer.id}</li>
                         <li class="list-group-item">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></li>
-                        <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.gitHub}">${engineer.gitHub}</a></li>
+                        <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.gitHub}" target="_blank">${engineer.gitHub}</a></li>
                     </ul>
                 </div>
             </div>
